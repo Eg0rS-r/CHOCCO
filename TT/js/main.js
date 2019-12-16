ff3766d8165354670c9dbf4eaffe028317c9e96b
Eg0r Sarychev <s4reg0r@gamil.com>
2019-12-16 21:07:19 +0500

    onepage scroll ready desktop

--- TT/js/main.js
+++ TT/js/main.js
@@ -3,19 +3,30 @@
 const wrap = $('.wrapper')
 const wrap_len = $('section').length
 const bar_link = $('.progress-bar__link')
+const menu_item_link = $('.menu__item')
 let top_wrap = 1
 
 console.log(bar_link)
 
 function scorll() {
   if ((wrap_len > top_wrap) && !(top_wrap < 0)) {
+    let top = top_wrap * (-100) + 'vh'
+    
     wrap.css('top', top_wrap * (-100) + 'vh')
     top_wrap = top_wrap + 1
     console.log(wrap.css('top'))
   }
 }
 
+
 bar_link.on('click', function (e) {
+  e.preventDefault();
+  top_wrap = $(this).index()
+  console.log(top_wrap)
+  scorll()
+})
+
+menu_item_link.on('click', function (e) {
   e.preventDefault();
   top_wrap = $(this).index()
   console.log(top_wrap)
@@ -30,7 +41,7 @@ wrap.on('wheel', function (e) {
     top_wrap = top_wrap - 2
     
   }
-  if (top_wrap == -1) {
+  if (top_wrap <= -1) {
     top_wrap =0
   }
   console.log(e.originalEvent)
